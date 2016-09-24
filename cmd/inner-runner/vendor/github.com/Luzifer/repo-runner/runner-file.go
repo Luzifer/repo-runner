e8f796891ded9e4a5ec8c657b7ab61317e13d1b4
Knut Ahlers <knut@ahlers.me>
2016-09-24 19:36:40 +0200

    Vendor deps

---
+++ cmd/inner-runner/vendor/github.com/Luzifer/repo-runner/runner-file.go
@@ -0,0 +1,85 @@
+package repo_runner
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	defaultRunnerFileLocation = ".repo-runner.yaml"
+	defaultFetchTimeout       = 5 * time.Second
+)
+
+type RunnerFile struct {
+	Image       string            `yaml:"image"`
+	Commands    []string          `yaml:"commands"`
+	Environment map[string]string `yaml:"environment"`
+}
+
+type ghFileResponse struct {
+	Content string `json:"content"`
+}
+
+func LoadFromGithub(repo, token string) (*RunnerFile, error) {
+	// https://developer.github.com/v3/repos/contents/#get-contents
+	// GET /repos/:owner/:repo/contents/:path
+
+	u := fmt.Sprintf("https://api.github.com/repos/%s/contents/%s",
+		repo, defaultRunnerFileLocation)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	if token != "" {
+		req.SetBasicAuth("auth", token)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultFetchTimeout)
+	defer cancel()
+
+	res, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Could not fetch %s file due to HTTP %d error.",
+			defaultRunnerFileLocation, res.StatusCode)
+	}
+
+	ghr := ghFileResponse{}
+	if err := json.NewDecoder(res.Body).Decode(&ghr); err != nil {
+		return nil, err
+	}
+
+	yamlData, err := base64.StdEncoding.DecodeString(ghr.Content)
+	if err != nil {
+		return nil, err
+	}
+
+	rf := &RunnerFile{}
+	return rf, yaml.Unmarshal(yamlData, rf)
+}
+
+func LoadFromFile(filename string) (*RunnerFile, error) {
+	if _, err := os.Stat(filename); err != nil {
+		return nil, err
+	}
+
+	yamlData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	rf := &RunnerFile{}
+	return rf, yaml.Unmarshal(yamlData, rf)
+}
